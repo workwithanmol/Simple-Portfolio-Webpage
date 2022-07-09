6c6e1fdece0d15fabcd2283df878f5ee46384bf9
workwithanmol <yadavanmol25999@gmail.com>
2022-07-09 09:27:21 +0545

    screenshot and few updates

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
-const hire = document.querySelectorAll('.hire_me')
-const arrow = document.querySelectorAll('.fa-arrow-up');
+// const hire = document.querySelectorAll('.hire_me')
+// const arrow = document.querySelectorAll('.fa-arrow-up');
 
-hire.addEventListener('mouseover', function(e) {
-    // console.log(e.target)
-    if (e.target.className == "hire_me") {
-        e.target.children.style.Color="#FFB742";
-    }
-});
+// let i=0
+// for (i = 0; i < hire.length; ++i) {
+// hire[i].addEventListener('mouseover', function(e) {
+//     // console.log(e.target)
 
-hire.addEventListener('mouseout', function(e) {
-    // console.log(e.target)
-    if (e.target.className == "hire_me") {
-        e.target.children.style.Color ="#212529";
-    }
-});
+//       if (e.target.className == "hire_me") {
+//         e.target.children.style.color="#FFB742";
+//       }
+//     })
+// }
+
+// for (i = 0; i < hire.length; ++i) {
+// hire[i].addEventListener('mouseover', function(e) {
+//     // console.log(e.target)
+
+//       if (e.target.className == "hire_me") {
+//         e.target.children.style.color="#FFB742";
+//       }
+//     })
+// }
+    
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+
